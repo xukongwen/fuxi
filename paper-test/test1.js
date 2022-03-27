48e69c80874c741bb4c95c4d3cad34f5d0a761fb
徐空文 <oabdao@gmail.com>
2022-03-27 14:11:42 +0800

    画出了基本阴阳图

--- paper-test/test1.js
+++ paper-test/test1.js
@@ -136,6 +136,40 @@ var gua6 = [[1, 1, 1, 1, 1, 1],
 
 
 
+
+// 画基本的横向阴阳渐变图 
+var topLeft = [20, 80];
+var bottomRight = [1000, 50];//这里竟然是增加的值
+
+// Create a rectangle shaped path between
+// the topLeft and bottomRight points:
+var path = new Path.Rectangle(topLeft, bottomRight);
+
+// 由什么颜色渐变成什么颜色
+var gradient = new Gradient(['white', 'black']);
+
+// 在何种方向和形状上渐变
+var gradientColor = new Color(gradient, topLeft, bottomRight);
+
+// 应用这个渐变设置
+path.fillColor = gradientColor;
+
+
+
+// 画基本的圆渐变，这是先天八卦的本意（吗）
+var circle = new Path.Circle(mousePoint, 200);
+var gradientColorCircle = new Color(gradient, [circle.position.x,circle.position.y-200], [circle.position.x,circle.position.y+200]);
+circle.strokeColor = gradientColorCircle;
+circle.strokeWidth = 50;
+
+
+
+
+
+
+
+
+
 //这是如何读json 
 var url = "data/gua-3.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
 var request = new XMLHttpRequest();
@@ -203,9 +237,9 @@ function draw_all_gua(x,y,list){
 
 
 
-draw_all_gua(mousePoint.x,10,gua1);
-draw_all_gua(mousePoint.x - (1*gap),(gap+box_length-10),gua2);
-draw_all_gua(mousePoint.x - (3*gap),200,gua3);
+//draw_all_gua(mousePoint.x,10,gua1);
+//draw_all_gua(mousePoint.x - (1*gap),(gap+box_length-10),gua2);
+//draw_all_gua(mousePoint.x - (3*gap),200,gua3);
 //draw_all_gua(10,200,gua6);
 
 
@@ -216,6 +250,19 @@ function onMouseDrag(event) {
 	//draw_all_gua(event.point.x,event.point.y,gua6);
 	//draw_yin(event.point.x,event.point.y)
 	//path.removeOnDrag();
+}
+
+
+// 现实鼠标所在坐标 item at {x: 30, y: 30}:
+var text = new PointText(new Point(30, 30));
+text.fillColor = 'black';
+
+text.content = '移动鼠标以查看位置坐标';
+
+function onMouseMove(event) {
+    // Each time the mouse is moved, set the content of
+    // the point text to describe the position of the mouse:
+    text.content = '鼠标坐标: ' + event.point.toString();
 }
 
 
@@ -238,6 +285,6 @@ function getCreateArcInfo(degrees,center,radius){
         strokeColor: 'black'
     }
 }
-var arcValues = getCreateArcInfo(1.5708, {x:253,y:334}, 160)
-var myArc = new Path.Arc(arcValues)
-myArc.strokeWidth = 10
+//var arcValues = getCreateArcInfo(1.5708, {x:253,y:334}, 160)
+//var myArc = new Path.Arc(arcValues)
+//myArc.strokeWidth = 10
